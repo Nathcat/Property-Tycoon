84d62132bf5b27a1ebbbc313334efcb667690168
SpaceDuckie <jaiden.hewitt.taylor@gmail.com>
2025-02-18 16:50:57 +0000

    Character controller closer to completion
    Added documentation
    Added dice rolling
    Still requiring character movement - needs branch 10.

--- Assets/Scripts/CounterController.cs
+++ Assets/Scripts/CounterController.cs
@@ -1,10 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// CounterController: A class used to control a counter, either controlled by a player or an AI.
+/// </summary>
 public class CounterController : MonoBehaviour
 {
+    /// <param name="portfolio"> The portfolio of the counter, containing any owned money and properties.</param>
     private Portfolio portfolio = new Portfolio();
+    private System.Random rnd = new System.Random();
 
     
     // Start is called before the first frame update
@@ -16,16 +23,24 @@ public class CounterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //PlayTurn();
     }
-
+    /// <summary>
+    ///     When called, the method rolls two 6 sided dice, outputting both results, along with the sum of the values.
+    /// </summary>
     public void PlayTurn()
     {
+        int dice1 = rnd.Next(1, 7);
+        Console.WriteLine("Dice 1 rolled a ", dice1);
+        int dice2 = rnd.Next(1, 7);
+        Console.WriteLine("Dice 2 rolled a ", dice1);
+        int output = dice1 + dice2;
+        Console.WriteLine("for a total of ", output);
 
+        /*
+        TODO: Add a block that moves the counter the correct number of spaces along the board - requires branch 10 to be pulled.
+        */
     }
 
-    public void SetSpace()
-    {
-
-    }
+    
 }
--- Assets/Scripts/Property.cs
+++ Assets/Scripts/Property.cs
@@ -2,25 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Property: A subclass of Asset, used to represent a property in PropertyTycoon.
+/// </summary>
 public class Property : Asset
 {
+    /// <param name="isMortgaged"> A boolean to signify if the property has been mortgaged.</param>
     private bool isMortgaged;
 
-    public Property()
+    /// <summary>
+    /// A constructor which takes an integer, and sets the value of the property to that, as well as setting the mortgage status to false.
+    /// </summary>
+    /// <param name="valueIn"> An integer denoting the value of the property.</param>
+    public Property(int valueIn)
     {
         isMortgaged = false;
+        value = valueIn;
     }
-
-    public int GetValue() 
-    {
-        return value;
-    }
-
+    /// <summary>
+    /// Returns the mortgage status of the property.
+    /// </summary>
+    /// <returns>True if mortgaged, False if not.</returns>
     public bool IsMortgaged()
     {
         return isMortgaged;
     }
-
+    /// <summary>
+    /// Sets the property for mortgage, and returns the amount of cash gained from doing so.
+    /// </summary>
+    /// <returns>Cash from mortgaging the property.</returns>
     public Cash Mortgage()
     {
         Cash output = new Cash();
