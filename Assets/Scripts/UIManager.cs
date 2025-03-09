169a95874513402e2931d3647070f1ec798299c9
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-03-09 14:08:25 +0000

    added example gameobject and some ui for main scene

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManager;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject StartScreen;
+
     // Start is called before the first frame update
     void Start()
     {
