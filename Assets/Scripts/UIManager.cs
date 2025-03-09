324e7912b6d53f5af4fca48b0b0378d3c327dd7d
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-03-09 15:56:59 +0000

    fixed typo

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -14,13 +14,14 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject Help;
     [SerializeField] private GameObject Rules;
     [SerializeField] private GameObject HelpAndEscape = null;
+    [SerializeField] private Scene scene;
     //private TMP_Text text;
-    Scene scene = SceneManager.GetActiveScene();
 
     // Start is called before the first frame update
     void Start()
     {
-        if (scene.name = "MainMenu")
+        Scene scene = SceneManager.GetActiveScene();
+        if (scene.name == "MainMenu")
         {
             StartScreen.SetActive(true);
             Credits.SetActive(false);
@@ -43,7 +44,7 @@ public class UIManager : MonoBehaviour
     //----------main menu----------
     public void BackButton() //This would be called by ALL back buttons on the main menu
     {
-        if (scene.name = "MainMenu")
+        if (scene.name == "MainMenu")
         {
             StartScreen.SetActive(true);
             HelpAndRules.SetActive(false);
