ab33ac31cfaf1fb0159026ed097acc5dd13f15d9
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-04-02 15:21:12 +0100

    Changed warning on Jail command

--- Assets/Scripts/DataManagement/Tokens/Jail.cs
+++ Assets/Scripts/DataManagement/Tokens/Jail.cs
@@ -7,6 +7,6 @@ public class Jail : Command
     public Jail(string value) : base(value) {}
     
     override public void Execute(CounterController counterController, Argument[] args) {
-        Debug.LogWarning("Should this command exist?");
+        Debug.LogWarning("Jail command does nothing, should it exist?");
     }
 }
