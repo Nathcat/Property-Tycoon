7b7dc901378030b53b5f38ec826de371bfdb4495
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-04-07 20:16:49 +0100

    Fixed issues left over from merge

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -24,16 +24,13 @@ public class CameraController : MonoBehaviour
     /// </summary>
     /// <param name="target">what you want the camera to focus on</param>
     void UpdateCamera(GameObject target)
-    void UpdateCamera(GameObject target)
     {
         //sets the position to that of the target
         this.transform.position = target.transform.position;
 
         //checks where the target is and then moves it based on if its in the N,S,E or W
         if (this.transform.position.x > boardRadius)
-        if (this.transform.position.x > boardRadius)
         {
-            this.transform.position = new Vector3(target.transform.position.x + sideOffset, target.transform.position.y + heightOffset, target.transform.position.z + lengthOffset);
             this.transform.position = new Vector3(target.transform.position.x + sideOffset, target.transform.position.y + heightOffset, target.transform.position.z + lengthOffset);
             this.transform.LookAt(target.transform.position);
         }
--- Assets/Scripts/DataManagement/Tokens/Command.cs
+++ Assets/Scripts/DataManagement/Tokens/Command.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+
 public class Command : Token
 {
     public Command(string value) : base(value) {}
