74ccc40308a34742b780431f728cc3f6417c9e69
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-04-09 00:09:35 +0100

    Reformed inheritance structure of `CounterController` to prepare for AI counter

---
+++ Assets/Scripts/Counter/HumanCounter.cs
@@ -0,0 +1,153 @@
+using System.Linq;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UIElements;
+
+
+/// <summary>
+/// CounterController: A class used to control a counter, either controlled by a player or an AI.
+/// </summary>
+public class HumanCounter : CounterController
+{
+
+    override public IEnumerator GoToJail()
+    {
+        MoveAbsolute(GameController.instance.jailSpace.position);
+
+        Debug.Log(name + " has gone to jail, can they pay?");
+
+        if (getOutOfJailFree)
+        {
+            Debug.Log("... they have a get out of jail free card!");
+            //Utils.RunAfter(1, GameController.instance.NextTurn);
+            yield return new WaitForSeconds(1f);
+        }
+
+        if (portfolio.GetCashBalance() >= 50)
+        {
+            Debug.Log("... they can pay, asking if they want to...");
+
+            yield return GameUIManager.instance.YesNoPrompt("Pay £50 to get out of jail?");
+            bool reply = GameUIManager.instance.promptState.response;
+
+            if (reply)
+            {
+                Cash fine = new Cash(50);
+                portfolio.RemoveCash(fine);
+                GameController.instance.freeParking.AddCash(fine);
+                Debug.Log(name + " pays to leave jail!");
+
+                //Utils.RunAfter(1, GameController.instance.NextTurn);
+            }
+            else
+            {
+                isInJail = true;
+                Debug.Log(name + " is now in jail! Jail space is at position " + GameController.instance.jailSpace.position);
+
+                //Utils.RunAfter(1, GameController.instance.NextTurn);
+            }
+        }
+        else
+        {
+            Debug.Log("... they cannot pay.");
+            isInJail = true;
+            Debug.Log(name + " is now in jail! Jail space is at position " + GameController.instance.jailSpace.position);
+            //Utils.RunAfter(1, GameController.instance.NextTurn);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    override public void LeaveJail()
+    {
+        isInJail = false;
+        turnsInJail = 0;
+        Debug.Log(name + " has left jail!");
+        //Utils.RunAfter(1, GameController.instance.NextTurn);
+    }
+
+    /// <summary>
+    /// Rolls both dice, and moves the counter. If the roll is a double roll, the dice are rolled again and counter moved again.
+    /// </summary>
+    override public IEnumerator PlayTurn()
+    {
+        if (!isInJail)
+        {
+            // Roll three times
+            yield return GameUIManager.instance.RollDice();
+
+            MoveCounter(lastRoll.dice1, lastRoll.dice2);
+            int doubles = 0;
+            while (lastRoll.doubleRoll)
+            {
+                doubles++;
+
+                if (doubles == 3)
+                {
+                    break;
+                }
+
+                yield return GameUIManager.instance.RollDice();
+
+                MoveCounter(lastRoll.dice1, lastRoll.dice2);
+            }
+
+            // If 3 doubles have been rolled, go to jail
+            if (doubles == 3)
+            {
+                Debug.Log(name + " has rolled 3 doubles, going to jail!");
+                yield return GoToJail();
+                yield break;
+            }
+
+            int oldPos = position;
+            do
+            {
+                oldPos = position;
+                yield return GameController.instance.spaces[position].action.Run(this);
+            } while (oldPos != position);
+
+
+            Space space = GameController.instance.spaces[position];
+
+            if (space is Property)
+            {
+                Property p = space as Property;
+
+                if (!p.isOwned && canPurchaseProperties)
+                {
+                    if (p.CanPurchase(this))
+                    {
+                        yield return GameUIManager.instance.YesNoPrompt("Would you like to buy " + p.name + " for £" + p.GetValue() + "?");
+
+                        bool reply = GameUIManager.instance.promptState.response;
+                        if (reply)
+                        {
+                            p.Purchase(this);
+                            Debug.Log(name + " has purchased " + p.name);
+                        }
+                        else
+                        {
+                            GameUIManager.instance.StartAuction();
+                        }
+                    }
+                    else
+                    {
+                        GameUIManager.instance.StartAuction();
+                    }
+                }
+            }
+        }
+        else
+        {
+            turnsInJail++;
+
+            Debug.Log(name + " has been in jail for " + turnsInJail + " turns");
+            if (turnsInJail == 2)
+            {
+                Debug.Log(name + " has done their time in jail!");
+                LeaveJail();
+            }
+        }
+    }
+}
