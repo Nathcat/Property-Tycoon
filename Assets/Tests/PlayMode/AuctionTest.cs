87e0c2618e38f708a9af6cf2025f5098bd69cfa1
josephholland07 <joeholland2107@gmail.com>
2025-04-09 20:34:37 +0100

    Auction Testing implementation
    despite the best efforts of auctioning, all current test ideas have been completed!

--- Assets/Tests/PlayMode/AuctionTest.cs
+++ Assets/Tests/PlayMode/AuctionTest.cs
@@ -8,8 +8,7 @@ using UnityEngine.TestTools;
 public class AuctionTest
 {
 
-    //work in progress atm, currently throws an error on line 28 of AuctionManager.cs
-    //[UnityTest]
+    [UnityTest]
     public IEnumerator TestBid()
     {
         SceneManager.LoadScene("Game");
@@ -20,8 +19,72 @@ public class AuctionTest
         PropertyGroup thisgroup = GameController.instance.groups[0];
         Property property = (Property)thisgroup.GetProperties()[0];
         CounterController counter1 = GameController.instance.turnCounter;
-        counter1.MoveAbsolute(12);
-        GameUIManager.instance.StartAuction();
+        counter1.portfolio.AddAsset(new Cash(20));
+        CounterController counter2 = GameController.instance.counters[1];
+        CounterController counter3 = GameController.instance.counters[2];
+        CounterController counter4 = GameController.instance.counters[3];
+        CounterController counter5 = GameController.instance.counters[4];
+        CounterController counter6 = GameController.instance.counters[5];
+                
+        AuctionManager auction = GameUIManager.instance.auctionManager;
+        auction.StartAuction(property);
+        foreach (var item in GameController.instance.counters)
+        {
+            if(item == counter1)
+            {
+                Debug.Log(counter1.portfolio.GetCashBalance());
+                auction.Bid1();
+            }
+            else
+            {
+                auction.Withdraw();
+            }
+        }
+                Assert.AreEqual(1, counter1.portfolio.GetProperties().Count);
+
+    }
+
+    [UnityTest]
+    public IEnumerator TestCompetingBids()
+    {
+        SceneManager.LoadScene("Game");
+        yield return null;
+        GameController.instance.SetupBoard();
+        GameController.instance.SetupCounters();
+
+        PropertyGroup thisgroup = GameController.instance.groups[0];
+        Property property = (Property)thisgroup.GetProperties()[0];
+        CounterController counter1 = GameController.instance.turnCounter;
+        counter1.portfolio.AddAsset(new Cash(20));
+        CounterController counter2 = GameController.instance.counters[1];
+        counter2.portfolio.AddAsset(new Cash(20));
+        CounterController counter3 = GameController.instance.counters[2];
+        CounterController counter4 = GameController.instance.counters[3];
+        CounterController counter5 = GameController.instance.counters[4];
+        CounterController counter6 = GameController.instance.counters[5];
+
+        AuctionManager auction = GameUIManager.instance.auctionManager;
+        auction.StartAuction(property);
+        foreach (var item in GameController.instance.counters)
+        {
+            if (item == counter1 || item == counter2)
+            {
+                if(item == counter1){
+                    auction.Bid1();
+                } else
+                {
+                    auction.Bid10();
+                }
+            }
+            else
+            {
+                auction.Withdraw();
+            }
+            
+        }
+        auction.Withdraw();
+
+        Assert.AreEqual(1, counter2.portfolio.GetProperties().Count);
 
     }
 }
