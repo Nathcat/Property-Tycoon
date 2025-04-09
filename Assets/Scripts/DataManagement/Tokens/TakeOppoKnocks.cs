339a13d0ad978834d27ab73c6fdea54c1e2471db
Nathcat <74734054+Nathcat@users.noreply.github.com>
2025-04-09 23:03:13 +0100

    Revert "nathan asked for this"
    This reverts commit 01dd983b30a49818ae66a0b4aefd37e2eeecc74d.

--- Assets/Scripts/DataManagement/Tokens/TakeOppoKnocks.cs
+++ Assets/Scripts/DataManagement/Tokens/TakeOppoKnocks.cs
@@ -8,19 +8,7 @@ public class TakeOppoKnocks : Command
     override public IEnumerator Execute(CounterController counterController, Argument[] args) {
         Debug.Log(counterController.name + " plays an Opportunity Knocks card");
         yield return GameController.instance.DrawOpportunity(counterController).action.Run(counterController);
-<<<<<<< Updated upstream
-        Settings.SetActive(true);
-=======
-        Settings.SetActive(true); 
->>>>>>> Stashed changes
-        public void MMSettingsButton()
-        {
-            StartScreen.SetActive(false);
-<<<<<<< Updated upstream
-            Settings.SetActive(true);
-=======
-        Settings.SetActive(true); 
->>>>>>> Stashed changes
-            yield break;
+
+        yield break;
     }
 }
