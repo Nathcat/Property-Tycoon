01dd983b30a49818ae66a0b4aefd37e2eeecc74d
lionbanana09 <139818521+lionbanana09@users.noreply.github.com>
2025-04-09 23:02:39 +0100

    nathan asked for this

--- Assets/Scripts/DataManagement/Tokens/TakeOppoKnocks.cs
+++ Assets/Scripts/DataManagement/Tokens/TakeOppoKnocks.cs
@@ -8,7 +8,19 @@ public class TakeOppoKnocks : Command
     override public IEnumerator Execute(CounterController counterController, Argument[] args) {
         Debug.Log(counterController.name + " plays an Opportunity Knocks card");
         yield return GameController.instance.DrawOpportunity(counterController).action.Run(counterController);
-
-        yield break;
+<<<<<<< Updated upstream
+        Settings.SetActive(true);
+=======
+        Settings.SetActive(true); 
+>>>>>>> Stashed changes
+        public void MMSettingsButton()
+        {
+            StartScreen.SetActive(false);
+<<<<<<< Updated upstream
+            Settings.SetActive(true);
+=======
+        Settings.SetActive(true); 
+>>>>>>> Stashed changes
+            yield break;
     }
 }
